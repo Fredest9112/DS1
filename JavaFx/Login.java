311b619284df8f6378c174f347c8ba0db1f71fab
fredest <71901890+Fredest9112@users.noreply.github.com>
2022-04-20 22:06:03 -0500

    Create Login.java

---
+++ JavaFx/Login.java
@@ -0,0 +1,16 @@
+public class Login {
+    private Almacen almacen;
+    
+    public Login(Almacen almacen){
+      this.almacen = almacen;
+    }
+    public boolean validar(String name, String pass) throws Exception {
+      String password = almacen.getUserPass(name);
+      if(password != null) {
+        Encriptador enc = new Encriptador();  
+        String myPass = enc.encriptar(pass);
+        return password.equals(myPass);
+      }
+      return false;   
+    }
+} 
